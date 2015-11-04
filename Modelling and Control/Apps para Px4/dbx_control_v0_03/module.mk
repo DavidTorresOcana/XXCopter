3b42f6df1666c8b417b55d665db4c30173ae0084
DavidTorresOcana <david.torres.ocana@gmail.com>
2015-11-04 14:24:28 +0000

    Wrapper par ala version V0.03 del dbx_control

---
+++ Modelling and Control/Apps para Px4/dbx_control_v0_03/module.mk
@@ -0,0 +1,27 @@
+MODULE_COMMAND = dbx_control
+
+SRCS = dbx_control_wrapper.c
+#SRCS +=dbx_control_ert_rtw/dbx_control.c
+#SRCS +=dbx_control_params.c
+
+
+#########
+START_DIR                 = C:/px4/Firmware-simulink/src/modules/dbx_control_v0_01
+MATLAB_ROOT               = C:/PROGRA~1/MATLAB/MATLAB~1/R2015a
+
+#SRCS += $(START_DIR)/dbx_control_ert_rtw
+#SRCS += $(START_DIR)
+#SRCS += $(MATLAB_ROOT)/extern/include
+#SRCS += $(MATLAB_ROOT)/extern/include
+#SRCS +=$(MATLAB_ROOT)/simulink/include 
+#SRCS +=$(MATLAB_ROOT)/simulink/include
+#SRCS +=$(MATLAB_ROOT)/simulink/include
+#SRCS +=$(MATLAB_ROOT)/simulink/include 
+#SRCS +=$(MATLAB_ROOT)/rtw/c/src 
+#SRCS +=$(MATLAB_ROOT)/rtw/c/src/ext_mode/common 
+#SRCS +=$(MATLAB_ROOT)/rtw/c/ert 
+#SRCS +=$(START_DIR)/slprj/ert/_sharedutils
+
+#SRCS += $(START_DIR)/dbx_control_ert_rtw/dbx_control_data.c
+
+
